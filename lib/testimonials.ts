def8e9bc373a1ab53d648497249552ed046851f2
arayer143 <89434892+arayer143@users.noreply.github.com>
2025-02-02 21:27:50 -0600

    added testimonials to the testimonials.ts file

--- lib/testimonials.ts
+++ lib/testimonials.ts
@@ -1,83 +1,154 @@
 export interface Testimonial {
-    name: string;
-    content: string;
-  }
-  
-  export const testimonials: Testimonial[] = [
-    {
-      name: "Denise Sutton",
-      content: "Clean Slate Pressure Washing Services truly exceeded my expectations! Their meticulous softwashing and pressure washing techniques brought new life to both my home and driveway. From the initial contact to the final rinse, their professionalism and attention to detail were evident. Not only did they effectively remove stubborn grime and buildup, but they also did so with care, ensuring the integrity of the surfaces. I highly recommend Clean Slate to anyone seeking top-notch pressure washing services. Five stars all the way!"
-    },
-    {
-      name: "Rebecca Rambin",
-      content: "I would highly recommend Clean Slate Pressure Washing Services. They did a fantastic job on my siding. Hard working, professional, courteous and experienced. The full package. Thank you, I love it."
-    },
-    {
-      name: "Cherise Foret",
-      content: "Rickey was awesome!! Very professional, nice and very communicative. He did such a wonderful job on our house, cement and shed. I would absolutely contact him again and recommend him to all of our family and friends!! Thank you so much! 😊"
-    },
-    {
-      name: "Marshall Candies",
-      content: "Our fence came out great. Very professional and kept us informed about arrival time and length of work. Will use them again when needed."
-    },
-    {
-      name: "Debbie Roberts",
-      content: "They did a wonderful job on my house, windows, driveway, sidewalk, and patio. Very pleased with the results. Highly recommend. Reasonable price. Thanks guys!"
-    },
-    {
-      name: "Nick Breaux",
-      content: "Great experience with clean slate!!! House looks brand new ! Fast ,prompt,and exceptional work! Would use and recommend 10 times over!!! Thanks again for bringing out home back to life."
-    },
-    {
-      name: "Donald Neiman",
-      content: "They did a great job of pressure washing and cleaning the exterior of my camper that had been outside for the past year. My camper looks brand new! I recommend them for any pressure washing needs that you have."
-    },
-    {
-      name: "Casey Hogan",
-      content: "The quote was fair, he was prompt to respond the questions. The job was completed with great results. Highly recommend using them again."
-    },
-    {
-      name: "Sallie Cortez",
-      content: "I recently hired Clean Slate Pressure Washing Services to soft wash my home, and the results were beyond impressive!"
-    },
-    {
-      name: "Gino Wright",
-      content: "Rickey with Clean Slate not only offered, but delivered, a great customer service experience in regards to pressure washing my home. My home is a mixture of brick and siding, and Rickey gave my house a beautiful shine just in time for Summer. Rickey even made himself available on a Sunday to take care of my home in the midst of my busy schedule. I definitely recommend Clean Slate's services for your home's washing or cleaning needs."
-    },
-    {
-      name: "Cynthia Savoie",
-      content: "Highly recommend Clean Slate ...he was very easy to talk to...professional and went beyond to help me move things around beings I am widowed and by myself...very punctual and communication on point...thank yall for yall service"
-    },
-    {
-      name: "Kenny Gallard",
-      content: "Clean slate pressure washing service did an excellent job washing my house and patio and even cleaned my gutters. I'm very satisfied with the professional and quality work Ricky provided.l would not only use him again but will tell all my friends and family if you want quality work at a reasonable price call clean slate."
-    },
-    {
-      name: "Badeel Mahdi",
-      content: "Best service ever,, they did a great and amazing job cleaning my house. Thank you so much Rickey's for making everything perfect."
-    },
-    {
-      name: "Jessica Mills",
-      content: "We had our home and driveway washed and was impressed with the quality of service and also the communication on scheduling and reminders. Thank you Ricky!"
-    },
-    {
-      name: "James Fish",
-      content: "Clean Slate pressure washed my driveway while I was on vacation! Their crew arrived on time and did a professional job!! Thanks!!!!"
-    },
-    {
-      name: "Dockranz",
-      content: "They did an awesome job on my driveway. Very professional. Very courteous. I highly recommend their services."
-    },
-    {
-      name: "Tevin Matthews",
-      content: "Clean slate is very professional company came on time and finished in a timely manner. My home still looks good after 4 weeks of being done"
-    },
-    {
-      name: "Danielle Hiltibidal",
-      content: "Clean Slate did an amazing job of making our house and driveway look brand new!"
-    },
-    {
-      name: "Ronald Jacob",
-      content: "Excellent work! Highly recommend this company"
-    }
-  ];
+  name: string
+  content: string
+}
+
+export const testimonials: Testimonial[] = [
+  {
+    name: "Denise Sutton",
+    content:
+      "Clean Slate Pressure Washing Services truly exceeded my expectations! Their meticulous softwashing and pressure washing techniques brought new life to both my home and driveway. From the initial contact to the final rinse, their professionalism and attention to detail were evident. Not only did they effectively remove stubborn grime and buildup, but they also did so with care, ensuring the integrity of the surfaces. I highly recommend Clean Slate to anyone seeking top-notch pressure washing services. Five stars all the way!",
+  },
+  {
+    name: "Rebecca Rambin",
+    content:
+      "I would highly recommend Clean Slate Pressure Washing Services. They did a fantastic job on my siding. Hard working, professional, courteous and experienced. The full package. Thank you, I love it.",
+  },
+  {
+    name: "Cherise Foret",
+    content:
+      "Rickey was awesome!! Very professional, nice and very communicative. He did such a wonderful job on our house, cement and shed. I would absolutely contact him again and recommend him to all of our family and friends!! Thank you so much! 😊",
+  },
+  {
+    name: "Marshall Candies",
+    content:
+      "Our fence came out great. Very professional and kept us informed about arrival time and length of work. Will use them again when needed.",
+  },
+  {
+    name: "Debbie Roberts",
+    content:
+      "They did a wonderful job on my house, windows, driveway, sidewalk, and patio. Very pleased with the results. Highly recommend. Reasonable price. Thanks guys!",
+  },
+  {
+    name: "Nick Breaux",
+    content:
+      "Great experience with clean slate!!! House looks brand new ! Fast ,prompt,and exceptional work! Would use and recommend 10 times over!!! Thanks again for bringing out home back to life.",
+  },
+  {
+    name: "Donald Neiman",
+    content:
+      "They did a great job of pressure washing and cleaning the exterior of my camper that had been outside for the past year. My camper looks brand new! I recommend them for any pressure washing needs that you have.",
+  },
+  {
+    name: "Casey Hogan",
+    content:
+      "The quote was fair, he was prompt to respond the questions. The job was completed with great results. Highly recommend using them again.",
+  },
+  {
+    name: "Sallie Cortez",
+    content:
+      "I recently hired Clean Slate Pressure Washing Services to soft wash my home, and the results were beyond impressive!",
+  },
+  {
+    name: "Gino Wright",
+    content:
+      "Rickey with Clean Slate not only offered, but delivered, a great customer service experience in regards to pressure washing my home. My home is a mixture of brick and siding, and Rickey gave my house a beautiful shine just in time for Summer. Rickey even made himself available on a Sunday to take care of my home in the midst of my busy schedule. I definitely recommend Clean Slate's services for your home's washing or cleaning needs.",
+  },
+  {
+    name: "Cynthia Savoie",
+    content:
+      "Highly recommend Clean Slate ...he was very easy to talk to...professional and went beyond to help me move things around beings I am widowed and by myself...very punctual and communication on point...thank yall for yall service",
+  },
+  {
+    name: "Kenny Gallard",
+    content:
+      "Clean slate pressure washing service did an excellent job washing my house and patio and even cleaned my gutters. I'm very satisfied with the professional and quality work Ricky provided.l would not only use him again but will tell all my friends and family if you want quality work at a reasonable price call clean slate.",
+  },
+  {
+    name: "Badeel Mahdi",
+    content:
+      "Best service ever,, they did a great and amazing job cleaning my house. Thank you so much Rickey's for making everything perfect.",
+  },
+  {
+    name: "Jessica Mills",
+    content:
+      "We had our home and driveway washed and was impressed with the quality of service and also the communication on scheduling and reminders. Thank you Ricky!",
+  },
+  {
+    name: "James Fish",
+    content:
+      "Clean Slate pressure washed my driveway while I was on vacation! Their crew arrived on time and did a professional job!! Thanks!!!!",
+  },
+  {
+    name: "Dockranz",
+    content:
+      "They did an awesome job on my driveway. Very professional. Very courteous. I highly recommend their services.",
+  },
+  {
+    name: "Tevin Matthews",
+    content:
+      "Clean slate is very professional company came on time and finished in a timely manner. My home still looks good after 4 weeks of being done",
+  },
+  {
+    name: "Danielle Hiltibidal",
+    content: "Clean Slate did an amazing job of making our house and driveway look brand new!",
+  },
+  {
+    name: "Ronald Jacob",
+    content: "Excellent work! Highly recommend this company",
+  },
+  {
+    name: "Emily Abry Barre",
+    content:
+      "Ricky with Clean Slate is extremely accommodating and dependable. He did a beautiful job soft washing my house, vinyl fence and pressure washing my sidewalks and driveway. Everything looks great! He also cleaned my gutters and dryer vents ~ things you need, but don't think about. Don't hesitate to call him ~ he is extremely prompt and reliable!",
+  },
+  {
+    name: "Alyssa Bourgeois",
+    content:
+      "10/10 recommend! Ricky is incredibly detail oriented and professional! Communication is top notch. They did an amazing job. Our home is white hardiplank, and we were having a hard time keeping it algae-free. They also pressure washed our driveway, porch, and walkway. Our house looks brand new, and we are so happy!",
+  },
+  {
+    name: "Lecourtney Evans",
+    content:
+      "Clean Slate Pressure Washing Services' did a spectacular job in cleaning our building. They were professional and very time oriented. Very informative and understand! Thank you so much for your time. We will definitely stay in contact.",
+  },
+  {
+    name: "Cornillus Thompson",
+    content: "Very professional and dome leave until the job is done.",
+  },
+  {
+    name: "Nicklaus Ruiz",
+    content: "Great communication. Punctual for appointment and very quick services.",
+  },
+  {
+    name: "Hayden",
+    content: "They washed my house and driveway. Rickey was so in Tuned with all my need.",
+  },
+  {
+    name: "Nurse Tosha",
+    content: "Very please with the service I received, I was in total shock how much my house transitioned.",
+  },
+  {
+    name: "Brandon Bacchus",
+    content:
+      "Did a great job on my house & fence… I recommend anybody that's looking for a great guy to pressure wash and work with your schedule and do what you asked. This is the guy.",
+  },
+  {
+    name: "Karen Hymel",
+    content: "First class job , finished very timely! Highly recommended",
+  },
+  {
+    name: "Tracy Triche",
+    content: "Very professional, didn't take long at all. Highly recommended.",
+  },
+  {
+    name: "Darryl Samaha",
+    content:
+      "Great job with fantastic customer service.! Highly recommend Clean Slate for all your pressure washing needs.",
+  },
+  {
+    name: "Elaine Hiltibidal",
+    content: "Awesome service and very reasonable price. Highly recommend",
+  },
+]
+
